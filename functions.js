831c645eefc79dc6816dcc818811b5137485d4ae
wajahat-dev <aliwajahat021@gmail.com>
2020-07-23 13:09:07 +0500

    obfus function.js

--- functions.js
+++ functions.js
@@ -1,87 +1,47 @@
+var $jscomp = $jscomp || {};
+$jscomp.scope = {};
+$jscomp.arrayIteratorImpl = function(a) { var b = 0; return function() { return b < a.length ? { done: !1, value: a[b++] } : { done: !0 } } };
+$jscomp.arrayIterator = function(a) { return { next: $jscomp.arrayIteratorImpl(a) } };
+$jscomp.makeIterator = function(a) { var b = "undefined" != typeof Symbol && Symbol.iterator && a[Symbol.iterator]; return b ? b.call(a) : $jscomp.arrayIterator(a) };
+
 function area() {
-    var RecLen, RecWid, Area;
-    RecLen = document.getElementById("areal ").value;
-    RecWid = document.getElementById("areaW ").value;
-    Area = RecLen * RecWid;
-    document.getElementById("areaR ").value = Area
-    console.log('area')
+    var a = document.getElementById("areal ").value;
+    var b = document.getElementById("areaW ").value;
+    a *= b;
+    document.getElementById("areaR ").value = a;
+    console.log("area")
 }
-
-
 
 function ratioAspect() {
-    var RecLen, RecWid, AspectRatio;
-    RecLen = document.getElementById("ratiol ").value;
-    RecWid = document.getElementById("ratioW ").value;
-    AspectRatio = RecLen / RecWid;
-    document.getElementById("ratioR ").value = AspectRatio
-    console.log('ratioAspect')
+    var a = document.getElementById("ratiol ").value;
+    var b = document.getElementById("ratioW ").value;
+    a /= b;
+    document.getElementById("ratioR ").value = a;
+    console.log("ratioAspect")
 }
-
-// Write a Temperature Conversion program. Allow the user to select 
-// the Unit for a temperature reading, then convert that reading into the target unit selected by the user.
-
 
 function TemperatureConverstion() {
-    const rbs = document.querySelectorAll('input[name="optradio"]');
-    let selectedValue;
-    // farenhit to celsisu
-    // C = 5/9 x (F-32)
-
-    var value = document.getElementById("value ").value;
-
-    // 0) farenhit
-    // 1) kelvin
-
-    var counter = 0;
-    for (const rb of rbs) {
-        if (rb.checked) {
-            if (counter == 0) {
-                var convertTofarenhist = (parseInt(value) * 9 / 5) + 32;
-                document.getElementById("conversion ").value = convertTofarenhist;
-            } else if (counter == 1) {
-                var convertTokelvin = parseInt(value) + 273.15;
-                document.getElementById("conversion ").value = convertTokelvin;
-            } else {
-                console.log("Error while conversing");
-            }
-            break;
-        }
-        counter += 1;
+    var a = document.querySelectorAll('input[name="optradio"]'),
+        b = document.getElementById("value ").value,
+        c = 0;
+    a = $jscomp.makeIterator(a);
+    for (var d = a.next(); !d.done; d = a.next()) {
+        if (d.value.checked) { 0 == c ? (b = 9 * parseInt(b) / 5 + 32, document.getElementById("conversion ").value = b) : 1 == c ? (b = parseInt(b) + 273.15, document.getElementById("conversion ").value = b) : console.log("Error while conversing"); break }
+        c += 1
     }
-
-
-    console.log(22);
+    console.log(22)
 }
 
-
-
-
-
-
-function pi_archimedes(n) {
-    var length = 2;
-    var polygon_sides = 2;
-    for (var i = 0; i < n; i++) {
-        length = 2 - (2 * Math.sqrt((1 - length / 4)));
-        polygon_sides *= 2;
-    }
-    return (polygon_sides * Math.sqrt(length));
-}
+function pi_archimedes(a) { for (var b = 2, c = 2, d = 0; d < a; d++) b = 2 - 2 * Math.sqrt(1 - b / 4), c *= 2; return c * Math.sqrt(b) }
 
 function calculation() {
-
-    // places = 3;
-    var places = document.getElementById("specifiedPrecision ").value;
+    var a = document.getElementById("specifiedPrecision ").value;
     old_result = null;
-    for (var n = 0; n < 10 * places; n++) {
-        result = pi_archimedes(parseInt(n).toPrecision(8));
+    for (var b = 0; b < 10 * a; b++) {
+        result = pi_archimedes(parseInt(b).toPrecision(8));
         result += parseInt(result).toPrecision(4);
-        // console.log('%d: %s', n, result.toFixed(places));
-        document.getElementById("pieResult ").value = parseFloat(result).toFixed(places);
-        if (result == old_result) {
-            break;
-        }
-        old_result = result;
+        document.getElementById("pieResult ").value = parseFloat(result).toFixed(a);
+        if (result == old_result) break;
+        old_result = result
     }
-}
+};
